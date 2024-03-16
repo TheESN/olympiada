46349e58d58393c76dea4f963b9772b12faed5e6
TheESN <sergmachine@gmail.com>
2024-03-16 15:02:40 +0900

    Employee endpoint

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from olymp.views import EmployeeViewSet
 
+api = [
+    path('getemployee/<int:id>', EmployeeViewSet.as_view())
+]
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include((api, 'olymp'), namespace='api'))
 ]
