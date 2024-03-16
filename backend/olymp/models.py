3883ce979d9caf90557a4b7185f1f49f0d859ab2
thatPolarLeafFox <AndreyChomch@mail.ru>
2024-03-16 12:20:46 +0900

    Recreated backend project

--- backend/olymp/models.py
+++ backend/olymp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Olympiada(models.Model):
+    olymp_name = models.CharField("Наименование", max_length=200, db_index=True)
+    olymp_date_start = models.DateField("Дата проведения")
+    olymp_time = models.TimeField("Длительность")
+    #olymp_creator = models.CharField(max_length=200)
