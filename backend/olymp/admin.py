dcede7521192cad16aab505ed9fc31ed8ccf585a
TheESN <sergmachine@gmail.com>
2024-03-16 13:04:42 +0900

    update user classes

--- backend/olymp/admin.py
+++ backend/olymp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Person, Employee, Student
 # Register your models here.
+admin.site.register(Person)
+admin.site.register(Employee)
+admin.site.register(Student)
