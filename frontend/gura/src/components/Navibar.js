c7f94b4510a393f57c02e7f56c6035300a01d310
guratsuki <bolspetr230@gmail.com>
2024-03-16 12:08:17 +0900

    sign up otdelno

--- frontend/gura/src/components/Navibar.js
+++ frontend/gura/src/components/Navibar.js
@@ -5,10 +5,15 @@ import './gg.css';
 
 export default function NaviBar(){
 
-    const [show, setShow] = useState(false);
+    const [showLog, setShowLog] = useState(false);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const handleCloseLog = () => setShowLog(false);
+    const handleShowLogin = () => setShowLog(true);
+
+    const [showSign, setShowSign] = useState(false);
+
+    const handleCloseSign = () => setShowSign(false);
+    const handleShowSign = () => setShowSign(true);
 
     return(
     <>
@@ -21,17 +26,43 @@ export default function NaviBar(){
                 <Nav.Link><Link to="/Record">Записаться</Link></Nav.Link>
             </Nav>
             <Nav>
-                <Button variant='primary' className='me-2' onClick={handleShow}>Войти</Button>
-                <Button variant='primary' onClick={handleShow}>Зарегистрироваться</Button>
+                <Button variant='primary' className='me-2' onClick={handleShowLogin}>Войти</Button>
+                <Button variant='primary' onClick={handleShowSign}>Зарегистрироваться</Button>
             </Nav>
         </Navbar.Collapse>
     </Navbar>
-    <Modal show={show} onHide={handleClose}>
+    <Modal show={showLog} onHide={handleCloseLog}>
         <Modal.Header closeButton>
             <Modal.Title>Вход</Modal.Title>
         </Modal.Header>
         <Modal.Body>
             <Form>
+                <Form.Group controlId="fromBasicEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type='email' placeholder='Введите email' />
+                </Form.Group>
+                <Form.Group controlId="fromBasicPassword">
+                    <Form.Label>Пароль</Form.Label>
+                    <Form.Control type='password' placeholder='Введите пароль' />
+                </Form.Group>
+
+            </Form>
+        </Modal.Body>
+    </Modal>
+    <Modal show={showSign} onHide={handleCloseSign}>
+        <Modal.Header closeButton>
+            <Modal.Title>Вход</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <Form>
+                <Form.Group controlId="fromBasicEmail">
+                    <Form.Label>Фамлилия</Form.Label>
+                    <Form.Control type='text' placeholder='Введите фамилию' />
+                </Form.Group>
+                <Form.Group controlId="fromBasicEmail">
+                    <Form.Label>Имя</Form.Label>
+                    <Form.Control type='text' placeholder='Введите имя' />
+                </Form.Group>
                 <Form.Group controlId="fromBasicEmail">
                     <Form.Label>Email</Form.Label>
                     <Form.Control type='email' placeholder='Введите email' />
