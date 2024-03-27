2740275066735dbce8be31b7830153bb5510ef0e
guratsuki <bolspetr230@gmail.com>
2024-03-27 17:13:56 +0900

    Table with json data

--- frontend/gura/src/App.js
+++ frontend/gura/src/App.js
@@ -1,26 +1,40 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import  NaviBar  from './components/Navibar';
+import db from "./data/olymp_list.json";
+import schema from "./data/schema.json";
+import Table from "./components/Table_new";
 
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
+  BrowserRouter as Router
 } from 'react-router-dom';
 
-import {List} from './List';
-import {Record} from './Record';
+import { useEffect, useState } from 'react';
+import { Button, Tab } from 'react-bootstrap';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    new Promise(resolve => {
+      setTimeout(() => {
+        resolve(db);
+      }, 1000);
+    }).then(result => {
+      setData(result);
+    });
+  });
   return (
     <>
     <Router>
     <NaviBar/>
-    <Routes>
-      <Route exact path="/" element={<List/>} />
-      <Route path="/record" element={<Record />} />
-    </Routes>
+    <div className="container p-2">
+      <div className="row">
+        <div className="col">
+          <Table headers={Object.keys(schema)} rows={data} />
+        </div>
+      </div>
+    </div>
     </Router>
     </>
   );
