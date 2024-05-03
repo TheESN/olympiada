22c97778729431c291a189b4cddeec93e2611cde
guratsuki <bolspetr230@gmail.com>
2024-05-03 14:38:32 +0900

    Edit/Delete added

--- frontend/gura/src/components/Table_three.js
+++ frontend/gura/src/components/Table_three.js
@@ -4,19 +4,37 @@ import axios from "axios"
 
 function JsonDataDisplay(id){
 	const [showModal, setShowModal] = useState(false);
+	const [editOlymp, setEditOlymp] = useState({
+		"id":-1,
+		"olymp_name":"a",
+		"olymp_date_start":"",
+		"olymp_time":""
+	}
+	);
 
-    const ShowWind = () => setShowModal(true)
-    const CloseWind = () => setShowModal(false)
+	//Показ модальное окно
+    const ShowWind = (event) => {
+		event.preventDefault();
+		
+		id = event.target.id;
+		let v = findOlympById(id);
+		setEditOlymp(v);
+		setShowModal(true)
+	}
+
+	//Закрыть модальное окно
+    const CloseWind = () => setShowModal(false)	
 
 	const [inputData, setInputData] = useState({name:''})
 
     const Clicked = (event) =>{
-        var id = event.target.id;
+		id = event.target.id
         console.log(id)
     }
 	
 	const [olymps, setOlymps] = useState([])
 
+	//Запрос списка олимпиады
 	useEffect(() => {
 		axios.get('http://localhost:8000/api/getolympiadas')
 		.then(res => {
@@ -24,17 +42,57 @@ function JsonDataDisplay(id){
 		})
 	}, [])
 
+	//Редактирование олимпиады
+	function handleSubmit(event){
+		event.preventDefault()
+
+		var url = "http://localhost:8000/api/getolympiada/" + editOlymp.id.toString();
+
+        axios.put(url, inputData)
+        .then(res => {
+            if (res.data.valid === true){
+                alert("Данные обновлены");
+                console.log(res.data.valid)
+            }
+            else{
+                alert("Неправильно введены данные");
+            }
+        })
+    }
+
+	//Удаление олимпиады
+	function DeleteSubmit(event){
+		event.preventDefault()
+
+		var url = "http://localhost:8000/api/getolympiada/" + editOlymp.id.toString();
+
+        axios.delete(url)
+        .then(res => {
+                alert("Удаленео");
+        })
+    }
+
+	//Вывод таблицы
 	const DisplayData = olymps.map((olymp) => {
 		return(
 			<tr>
 				<td>{olymp.id}</td>
-				<td><a href='#' onClick={ShowWind}>{olymp.olymp_name}</a></td>
+				<td><a href='#' onClick={ShowWind} id={olymp.id}>{olymp.olymp_name}</a></td>
 				<td>{olymp.olymp_date_start}</td>
 			    <td>{olymp.olymp_time}</td>
 			    <td><Button variant='primary' onClick={Clicked} id={olymp.id}>Записаться</Button></td>
 			</tr>
 		)
 	})
+
+	//Поиск олимпиады по айди
+	function findOlympById(ID) {
+		for(var i=0;i<olymps.length;i++){
+			if (olymps[i].id == ID){		
+				return olymps[i];
+			}
+	    } 
+	}
 
 	return(
         <>
@@ -60,20 +118,21 @@ function JsonDataDisplay(id){
             </Modal.Header>
             <Modal.Body>
                 <Form>
+					{/* <Form.Control type='hidden' value={editOlymp.id}/> */}
                     <Form.Group>
                         <Form.Label>Название</Form.Label>
-                            <Form.Control type='text' name="olymp_name" value={DisplayData}/>
+                            <Form.Control type='text' name="olymp_name" onChange={e => setInputData({...inputData, olymp_name: e.target.value})}  required/>
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Дата начала</Form.Label>
-                        <Form.Control type='datefield'  name="olymp_date_start"/>
+                        <Form.Control type='datefield'  name="olymp_date_start" onChange={e => setInputData({...inputData, olymp_date_start: e.target.value})} required/>
                     </Form.Group>
                     <Form.Group>
                         <Form.Label>Длительность</Form.Label>
-                        <Form.Control type='time'  name="olymp_time"/>
+                        <Form.Control type='time'  name="olymp_time" onChange={e => setInputData({...inputData, olymp_time: e.target.value})} required/>
                     </Form.Group>
-                    <Button className='mt-3' type="submit" >Обновить</Button>
-					<Button variant='danger' className='ms-2 mt-3' type='submit'>Удалить</Button>
+                    <Button className='mt-3' type="submit" onClick={handleSubmit}>Обновить</Button>
+					<Button variant='danger' className='ms-2 mt-3' type='submit' onClick={DeleteSubmit}>Удалить</Button>
                 </Form>
             </Modal.Body>
         </Modal>
