48e6d798655f36a30980a3ea4456d03f2a6f7620
TheESN <sergmachine@gmail.com>
2024-05-04 12:11:35 +0900

    Begin of Applications

--- backend/olymp/admin.py
+++ backend/olymp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Olympiada, Person, Employee, Student
+from .models import Olympiada, Person, Employee, Student, Application
 
 # Register your models here.
 admin.site.register(Olympiada)
 admin.site.register(Person)
 admin.site.register(Employee)
 admin.site.register(Student)
+admin.site.register(Application)
